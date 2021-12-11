b8c3964b4b8493550af3235228f5a154f6e49ab8
thecodinghyrax <thecodinghyrax@gmail.com>
2021-12-11 08:16:19 -0600

    Add project files.

---
+++ FinalProject/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using FinalProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.FV = 0;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(CalculatorModel model)
+        {
+            if (ModelState.IsValid)
+            {
+            ViewBag.FV = model.CalculateFutureValue();
+            }
+            else
+            {
+                ViewBag.FV = 0;
+            }
+            return View(model);
+        }
+    }
+}
